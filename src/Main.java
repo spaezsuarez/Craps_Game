0ee2f3339f5f8c0d3be3027a7cab00fba6ec1bba
spaezsuarez <spaezsuarez@gmail.com>
2021-05-20 13:45:15 -0500

    Llamada singleton MenuInicio

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import views.MenuInicio;
 public class Main {
     
     public static void main(String[] args) {
-        MenuInicio menu = new MenuInicio();
+        MenuInicio menu = MenuInicio.getInstance();
         menu.initTemplate();
     }
     
