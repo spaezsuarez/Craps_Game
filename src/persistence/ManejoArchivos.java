05b036d633e5e657eb29c91ec60394783ff6a67e
spaezsuarez <spaezsuarez@gmail.com>
2021-05-20 23:25:45 -0500

    Metodos para la lectura e inserción de datos en un txt

--- src/persistence/ManejoArchivos.java
+++ src/persistence/ManejoArchivos.java
@@ -1,18 +1,53 @@
 package persistence;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import models.Jugador;
+
 public class ManejoArchivos {
 
-   private static ManejoArchivos instance;
-   
-   private ManejoArchivos(){
-       
-   }
-   
-   public static ManejoArchivos getInstance(){
-       if(instance == null){
-           instance = new ManejoArchivos();
-       }
-       return instance;
-   }
+    private static ManejoArchivos instance;
+    private String rutaArchivo;
+
+    private ManejoArchivos() {
+        rutaArchivo = "./src/persistence/data.txt";
+    }
+
+    public String[] obtenerDatosJugador(String nombre,String contraseña) throws FileNotFoundException, IOException {
+        String[] datosJugador = new String[3];
+        String cadena;
+        FileReader f = new FileReader(rutaArchivo);
+        BufferedReader b = new BufferedReader(f);
+        while ((cadena = b.readLine()) != null) {
+            datosJugador = cadena.split(",");
+            if(datosJugador[0].equals(nombre) && datosJugador[1].equals(contraseña)){
+               return datosJugador;
+            }
+        }
+        b.close();
+        datosJugador = null;
+        return datosJugador;
+    }
+
+    public void registrarDatosJugador(Jugador jugador) throws IOException {
+        File archivo = new File(rutaArchivo);
+        BufferedWriter bw;
+        bw = new BufferedWriter(new FileWriter(archivo, true));
+        bw.write(jugador.toString() + "\n");
+        bw.close();
+
+    }
+
+    public static ManejoArchivos getInstance() {
+        if (instance == null) {
+            instance = new ManejoArchivos();
+        }
+        return instance;
+    }
 
 }
