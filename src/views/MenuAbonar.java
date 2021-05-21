48d48887f5ff21fe669b97090e7405f0baa0880c
spaezsuarez <spaezsuarez@gmail.com>
2021-05-21 16:45:14 -0500

    Creación de la clase Menu Abonar

---
+++ src/views/MenuAbonar.java
@@ -0,0 +1,21 @@
+package views;
+
+
+public class MenuAbonar extends Menu {
+    
+    
+    public MenuAbonar(){
+        super.ANCHO = 600;
+        super.ALTO = 400;
+    }
+    
+    protected void initComponents() {
+       
+    }
+    
+    protected void initListeners(){
+        
+    }
+    
+    
+}
