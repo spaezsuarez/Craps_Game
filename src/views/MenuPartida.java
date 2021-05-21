430767da35fcd6d26a8ef6a36db99aa9432159a1
spaezsuarez <spaezsuarez@gmail.com>
2021-05-21 13:42:36 -0500

    Uso de los modelos en el menu de la Partida

--- src/views/MenuPartida.java
+++ src/views/MenuPartida.java
@@ -13,17 +13,27 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+import java.io.IOException;
 
+import models.Partida;
+import models.Jugador;
+import persistence.ManejoArchivos;
 
 public class MenuPartida extends JFrame {
-    
-    private final int ANCHO = 600,ALTO = 500;
-    
-    private JLabel dadoUno,dadoDos,datosJugador;
+
+    private final int ANCHO = 600, ALTO = 500;
+
+    private JLabel dadoUno, dadoDos, datosJugador;
     private JTextField inputApuesta;
-    private JButton btnJugar;
-    
-    
+    private JButton btnJugar, btnVolver;
+
+    private Partida partida;
+
+    public MenuPartida(String[] dataPlayer) {
+        partida = new Partida(new Jugador(dataPlayer[0], dataPlayer[1], Double.parseDouble(dataPlayer[2])));
+    }
+
     private void setImagen(JLabel label, String nombreImg) {
         String rutaBase = "src/resources/game/" + nombreImg;
         ImageIcon instr = new ImageIcon(rutaBase);
@@ -32,58 +42,111 @@ public class MenuPartida extends JFrame {
         ImageIcon imagen = new ImageIcon(nuevaimagen);
         label.setIcon(imagen);
     }
-    
-    private void setDataPlayer(String nombre,String saldo){
-        String data = "<html><body><p style='padding:10px;'>Nombre: "+nombre+"</p><br><p>Saldo:"+saldo+"</p><body></html>";
+
+    private void setDataPlayer(String nombre, String saldo) {
+        String data = "<html><body><p style='padding:10px;'>Nombre: " + nombre + "</p><br><p>Saldo:" + saldo + "</p></body></html>";
         datosJugador.setText(data);
         repaint();
     }
-    
-    private void initComponents(){
-        datosJugador = new JLabel("<html><body style='padding:10px;'><p>Nombre: "+"</p><br><p>Saldo:"+"</p><body></html>");
-        datosJugador.setSize(new Dimension(300,100));
+
+    private void initComponents() {
+        datosJugador = new JLabel("<html><body style='padding:10px;'><p>Nombre: " + "</p><br><p>Saldo:" + "</p></body></html>");
+        datosJugador.setSize(new Dimension(300, 100));
         datosJugador.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        datosJugador.setLocation((this.getWidth()-datosJugador.getWidth()) /2, 50);
+        datosJugador.setLocation((this.getWidth() - datosJugador.getWidth()) / 2, 50);
         add(datosJugador);
-        
+
         inputApuesta = new JTextField();
-        inputApuesta.setSize(new Dimension(200,20));
+        inputApuesta.setSize(new Dimension(200, 20));
         inputApuesta.setHorizontalAlignment(JTextField.CENTER);
-        inputApuesta.setFont(new Font("Arial",Font.PLAIN,15));
-        inputApuesta.setLocation((this.getWidth()-inputApuesta.getWidth())/2+100, 200);
+        inputApuesta.setFont(new Font("Arial", Font.PLAIN, 15));
+        inputApuesta.setLocation((this.getWidth() - inputApuesta.getWidth()) / 2 + 100, 200);
         add(inputApuesta);
-        
+
         JLabel label = new JLabel("Valor de la apuesta: ");
-        label.setSize(new Dimension(300,20));
-        label.setLocation((this.getWidth()-inputApuesta.getWidth())/2-100, 200);
+        label.setSize(new Dimension(300, 20));
+        label.setLocation((this.getWidth() - inputApuesta.getWidth()) / 2 - 100, 200);
         add(label);
-        
+
         dadoUno = new JLabel();
-        dadoUno.setSize(new Dimension(100,100));
-        dadoUno.setLocation((this.getWidth() - dadoUno.getWidth()) /2 -100, 270);
+        dadoUno.setSize(new Dimension(100, 100));
+        dadoUno.setLocation((this.getWidth() - dadoUno.getWidth()) / 2 - 100, 270);
         setImagen(dadoUno, "dado1.png");
         add(dadoUno);
-        
+
         dadoDos = new JLabel();
-        dadoDos.setSize(new Dimension(100,100));
-        dadoDos.setLocation((this.getWidth() - dadoDos.getWidth()) /2 +100, 270);
+        dadoDos.setSize(new Dimension(100, 100));
+        dadoDos.setLocation((this.getWidth() - dadoDos.getWidth()) / 2 + 100, 270);
         setImagen(dadoDos, "dado1.png");
         add(dadoDos);
-        
+
         btnJugar = new JButton("Tirar");
         btnJugar.setSize(100, 30);
         btnJugar.setFocusable(false);
         btnJugar.setBackground(new Color(51, 157, 179));
         btnJugar.setForeground(Color.WHITE);
-        btnJugar.setFont(new Font("Arial",Font.PLAIN,20));
-        btnJugar.setLocation((this.getWidth()-btnJugar.getWidth())/2, 400);
+        btnJugar.setFont(new Font("Arial", Font.PLAIN, 20));
+        btnJugar.setLocation((this.getWidth() - btnJugar.getWidth()) / 2, 400);
         add(btnJugar);
+
+        btnVolver = new JButton("<html><body>&#129044;</body></html>");
+        btnVolver.setSize(100, 30);
+        btnVolver.setBackground(new Color(51, 157, 179));
+        btnVolver.setForeground(Color.WHITE);
+        btnVolver.setFont(new Font("Arial", Font.PLAIN, 20));
+        btnVolver.setLocation(10, ALTO - 70);
+        btnVolver.setFocusable(false);
+        add(btnVolver);
     }
-    
-    private void initListeners(){
+
+    private void initListeners() {
         btnJugar.addActionListener((event) -> {
-            MenuInicio.getInstance().initTemplate();
-            dispose();
+            try {
+                if (Double.parseDouble(inputApuesta.getText()) <= partida.getJugador().getSaldo()) {
+                    ManejoArchivos insManejoArchivos = ManejoArchivos.getInstance();
+                    partida.setValorApuesta(Double.parseDouble(inputApuesta.getText()));
+                    int[] result = partida.jugarRonda();
+
+                    setImagen(dadoUno, "dado" + result[0] + ".png");
+                    setImagen(dadoUno, "dado" + result[1] + ".png");
+                    System.out.println("Result: " + result[2]);
+                    repaint();
+
+                    partida.determinarResultado();
+
+                    if (partida.isResult() != null && partida.isResult().equals(Boolean.TRUE)) {
+                        JOptionPane.showMessageDialog(null, "Gano", "Felicidades", JOptionPane.INFORMATION_MESSAGE);
+                        partida.modificarSaldoJugador();
+                        setDataPlayer(partida.getJugador().getNombre(), "" + partida.getJugador().getSaldo());
+                        repaint();
+                        insManejoArchivos.editarDatosJugador(partida.getJugador());
+
+                        setImagen(dadoUno, "dado1.png");
+                        setImagen(dadoUno, "dado1.png");
+                        inputApuesta.setText("");
+
+                        partida.setValorInicial(null);
+                        partida.setValorSecundario(null);
+
+                        repaint();
+
+                    } else if (partida.isResult() != null && partida.isResult().equals(Boolean.FALSE)) {
+                        JOptionPane.showMessageDialog(null, "Perdio", "Lo sentimos", JOptionPane.ERROR_MESSAGE);
+                        partida.modificarSaldoJugador();
+                        setDataPlayer(partida.getJugador().getNombre(), "" + partida.getJugador().getSaldo());
+                        repaint();
+                        insManejoArchivos.editarDatosJugador(partida.getJugador());
+                    }
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "Asegurese que tiene suficient dinero para apostar", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Tiene que haber un valor de apuesta", "Error", JOptionPane.ERROR_MESSAGE);
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "Ha ocurrido un error al guardar los datos", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+
         });
 
         btnJugar.addMouseListener(new MouseAdapter() {
@@ -100,9 +163,29 @@ public class MenuPartida extends JFrame {
                 btnJugar.setForeground(Color.WHITE);
             }
         });
+
+        btnVolver.addActionListener((event) -> {
+            MenuInicio.getInstance().initTemplate();
+            dispose();
+        });
+
+        btnVolver.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent evt) {
+                btnVolver.setBackground(Color.WHITE);
+                btnVolver.setForeground(Color.BLACK);
+                btnVolver.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent evt) {
+                btnVolver.setBackground(new Color(51, 157, 179));
+                btnVolver.setForeground(Color.WHITE);
+            }
+        });
     }
-    
-    public void initTempalte(){
+
+    public void initTempalte() {
         setLayout(null);
         Image icon = new ImageIcon(getClass().getResource("/resources/menus/dados.png")).getImage();
         setIconImage(icon);
@@ -114,8 +197,9 @@ public class MenuPartida extends JFrame {
         setLocationRelativeTo(null);
         initComponents();
         initListeners();
+        setDataPlayer(partida.getJugador().getNombre(), "" + partida.getJugador().getSaldo());
         setVisible(true);
-        
+
     }
-    
+
 }
